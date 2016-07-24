07bd1df5e3009488b32a3c0c9a90d35d99ba79a8
alan <alan@fancysandwiches.com>
2016-07-24 15:04:28 -0700

    Rename DecimalEncoder to ColumnEncoder
    - I'm guessing we're going to have a problem with other column types in the future, so I figured it was best to rename it now.

--- grice/column_encoder.py
+++ grice/column_encoder.py
@@ -3,9 +3,10 @@ from decimal import Decimal
 from flask.json import JSONEncoder
 
 
-class DecimalEncoder(JSONEncoder):
+class ColumnEncoder(JSONEncoder):
     """
-    This encoder is required in order for us to serialize "real" column types.
+    This encoder is required in order for us to serialize "real" column types from SQLAlchemy. We will likely need to
+    add support for more columns later.
     """
     def default(self, obj):
         try:
